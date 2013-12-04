e4e57a1081e98ac5b3109a093ea2165720aa89bd
s_murakami <s.murakami@conol.jp>
2013-12-04 11:38:58 +0900

    draw pointを改良

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(function(){
-    var drawPoint = function(ctx, x, y, in_the_circle){
+    // 点の描画
+    var drawPoint = function(ctx, x, y, in_the_circle, radius){
+        if (typeof(radius) === "undefined") radius = 2;
         ctx.beginPath();
         if(in_the_circle)ctx.fillStyle = 'rgba(192, 80, 77, 0.7)'; // 赤
         else ctx.fillStyle = 'rgba(155, 187, 89, 0.7)'; // 緑
-        ctx.arc(x, y, 2, 0, Math.PI * 2, false);
+        ctx.arc(x, y, radius, 0, Math.PI * 2, false);
         ctx.fill();
     };
 
